ba8684ce05c66412473b5c05e45c0a6d69972a16
vincent.wen <wenziyangday@gmail.com>
2021-12-13 16:53:45 +0800

    update: Todo 源码

--- src/labs/todo/Todo.tsx
+++ src/labs/todo/Todo.tsx
@@ -1,5 +1,6 @@
 import "@labs/todo/style/todo.less";
 
+import TodoCoreHook from "@labs/todo/components/TodoCoreHook";
 import TodoHook from "@labs/todo/components/TodoHook";
 import { Card, Divider } from "antd";
 import React, { FC } from "react";
@@ -8,6 +9,10 @@ const Todo: FC = () => {
   return (
     <Card className="todo-list" title="每周TODO">
       <h3>每周安排自己做些小玩意</h3>
+
+      {/** react hooks的源码认识 */}
+      <TodoCoreHook />
+      <Divider />
 
       {/** react hooks的基本认识和使用 */}
       <TodoHook />
--- src/labs/todo/components/TodoCoreHook.tsx
+++ src/labs/todo/components/TodoCoreHook.tsx
@@ -1,9 +1,16 @@
+import { List } from "antd";
 import React, { FC } from "react";
 
 const TodoCoreHook: FC = () => {
   return (
     <div>
       <h1>TIME: 12.13~</h1>
+      <div>
+        <h3>1.源码</h3>
+        <List>
+          <List.Item.Meta title="useRef" description={<div>2323</div>} />
+        </List>
+      </div>
     </div>
   );
 };
