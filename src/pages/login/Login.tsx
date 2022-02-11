491e8e88d529e4634794d5afaabda4e7ac45b4e3
vincent.wen <vincent.wen@wayz.ai>
2022-02-11 16:36:59 +0800

    update: login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,14 +1,30 @@
 import "@pages/login/style/login.less";
 
+import { Button, Form, Input } from "antd";
 import React, { FC } from "react";
 
 const Login: FC = () => {
   return (
-    <div className="login">
-      <div className="i-wrap">
-        <div className="i-w-title">登录</div>
+    <Form
+      layout="vertical"
+      onFinish={(values: any) => {
+        console.log(values);
+      }}
+    >
+      <div className="login">
+        <div className="i-wrap">
+          <Form.Item className="Item" label="用户名：" name="userName">
+            <Input />
+          </Form.Item>
+          <Form.Item className="Item" label="密   码：" name="password">
+            <Input />
+          </Form.Item>
+          <Form.Item>
+            <Button htmlType="submit">登录</Button>
+          </Form.Item>
+        </div>
       </div>
-    </div>
+    </Form>
   );
 };
 export default Login;
